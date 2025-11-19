cb5c98adc03a70e4a26a944bf4639da5b0ef7277
ewang368 <ewang368@uwo.ca>
2025-11-19 16:55:36 -0500

    Add files via upload

---
+++ classNotation.js
@@ -0,0 +1,11 @@
+/**
+ * Define the Course and Assignment concepts using the class notations
+ */
+
+// create the objects using the classes
+
+let c1; 
+let c2;
+
+c1.courseInfo();
+c2.courseInfo();
